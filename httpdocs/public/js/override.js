b96075a0cfbecffc8e12fca43a3300b04e718665
Elliot J. Voris <elliot@voris.me>
2016-11-28 15:40:52 -0600

    Let's Add These Buttons, bruh!
    Adding a couple links to "more {announcements,events}", respectively.

--- httpdocs/public/js/override.js
+++ httpdocs/public/js/override.js
@@ -1,23 +1,2 @@
-/*
-$(document).ready(function () {
-    if(window.location.href.indexOf("online-giving") > -1) {
-       console.log("your url contains the name online-giving");
-       var article = document.getElementsByTagName('article')[0];
-       var script1 = document.createElement('script');
-       var div = document.createElement('div');
-       var script2 = document.createElement('script');
-       var node = document.createTextNode("require('osg/osg').start('sf7f38r', '#eb5e42', 'Noto Serif', '400', '5px')");
-       script1.type = 'text/javascript';
-       script1.src = 'https://give.church/osg.js';
-       div.class = 'kindrid-osg-button';
-       script2.type = 'text/javascript';
-       script2.appendChild(node);
-       article.appendChild(script1);
-       article.appendChild(div);
-       article.appendChild(script2);
-    }
-});
-$(document).ready(function () {
-  window.alert("Thanks for visiting our website!\nGod bless you.");
-})
-*/
+$('.blog-widget').append('<a class="fancy-button blog-widget-more-link" href="/announcements">More Announcements</a>');
+$('.events-widget').append('<a class="fancy-button events-widget-more-link" href="/events">More Events</a>');
