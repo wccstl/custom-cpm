9982a1764e5bf1b235c55ed76e441d398071dc2c
Elliot J. Voris <elliot@voris.me>
2017-01-11 16:35:07 -0600

    Automated the weather alert slides
    The javascript searches to see if any of the alert pages have been
    posted. Then, it sticks in a slide to the homepage rotator that
    corresponds.

---
+++ httpdocs/public/js/backup-override.js
@@ -0,0 +1,2 @@
+$('.blog-widget').append('<a class="fancy-button blog-widget-more-link" href="/announcements">More Announcements</a>');
+$('.events-widget').append('<a class="fancy-button events-widget-more-link" href="/events">More Events</a>');
